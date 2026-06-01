077d7cce34efd7cd7dec254fafd5d06d7e6a846f
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-06-01 10:14:42 -0300

    se mejoro el orden de la imagen, su tamaño y se dividio la carta, tambien se agrego un main donde se coloco el div

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,20 +10,23 @@ const personajes = [
 { id: 5, nombre: "Abraxas", imagen:
 "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg" },
 ];
+const nuevoarreglo = [personajes]
 
 const contenedor = document.getElementById("contenedor");
 function renderizar(personajes) {
     for(let i = 0; i < personajes.length; i++) {
     contenedor.innerHTML += `
-        <div class="card">
-            <img src="${personajes[i].imagen}">
+    <div class="col-3 my-3">
+    <div class="card" style= "width: 18rem">
+            <img src="${personajes[i].imagen}"
+            >
+            <div class="card-body">
             <h5>${personajes[i].nombre}</h5>
-            <button class="btn btn-primary">
-            Mostrar
             </button>
-            <button class="btn btn-danger">
+            <button class="btn btn-danger boton-eliminar">
             Eliminar
             </button>
+        </div>
         </div>
     `;
 }
